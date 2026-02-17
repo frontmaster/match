4ab27b03d1dbf78032c89602333e60d7ac7a82eb
shun-front <ogasawarashun49@gmail.com>
2026-02-17 22:18:19 +0900

    フロント部分修正

--- src/resources/js/Pages/ProjectList.vue
+++ src/resources/js/Pages/ProjectList.vue
@@ -1,21 +1,24 @@
 (<template>
   <div class="p-mypage__itemContainer">
-    <h1 class="text-2xl font-bold mb-4">案件一覧</h1>
     <div v-for="p in projects" :key="p.id" :project="p">
-      <p>案件名：{{ p.project_title }}</p>
-      <p>
-        案件種別：{{
-          p.project_type === "single" ? "単発" : "レベニューシェア"
-        }}
-      </p>
       <div class="p-mypage__projectList">
-        <p v-if="p.project_type === 'single'">価格：{{ p.price_min }}</p>
-        <span>千円</span>
-        <span>〜</span>
-        <p v-if="p.project_type === 'single'">{{ p.price_max }}</p>
-        <span>千円</span>
+        <p>案件名：{{ p.project_title }}</p>
+        <p>
+          案件種別：{{
+            p.project_type === "single" ? "単発" : "レベニューシェア"
+          }}
+        </p>
+        <div class="p-mypage__item">
+          <p v-if="p.project_type === 'single'">価格：{{ p.price_min }}</p>
+          <span v-if="p.project_type === 'single'">千円</span>
+          <span v-if="p.project_type === 'single'">〜</span>
+
+          <p v-if="p.project_type === 'single'">{{ p.price_max }}</p>
+          <span v-if="p.project_type === 'single'">千円</span>
+        </div>
+
+        <p>内容：{{ p.content }}</p>
       </div>
-      <p>内容：{{ p.content }}</p>
     </div>
   </div>
 </template>
--- src/resources/views/prof/create.blade.php
+++ src/resources/views/prof/create.blade.php
@@ -3,45 +3,54 @@
 @section('title', 'プロフィール編集ページ')
 
 @section('content')
+<h1 class="p-profile__title">プロフィール編集</h1>
 <div class="p-profile">
-    <h1 class="p-profile__title">プロフィール編集</h1>
     @if(session('success'))
     <div>
         {{ session('success') }}
     </div>
     @endif
-    <form action="{{ route('profiles.update', $user->id) }}" method="POST" enctype="multipart/form-data" class="p-profile__form">
-        @csrf
-        @method('PUT')
+    <div class="p-profile__formContainer">
+        <form action="{{ route('profiles.update', $user->id) }}" method="POST" enctype="multipart/form-data" class="p-profile__form">
+            @csrf
+            @method('PUT')
 
-        @error('image')
-        <div class="p-profile__errMsg c-errMsg">{{ $message }}</div>
-        @enderror
-        <div class="p-profile__part">
-            <label for="" class="p-profile__label">プロフィール画像</label>
-            <img src="{{ $user->image ? asset('storage/img/' . $user->image) : asset('img/person.jpg') }}" class="p-profile__img" alt="プロフィール画像">
-            <input type="file" name="image">
-        </div>
+            @error('image')
+            <div class="p-profile__errMsg c-errMsg">{{ $message }}</div>
+            @enderror
+            <div class="p-profile__itemContainer">
+                <div class="p-profile__item">
+                    <label for="" class="p-profile__label">プロフィール画像</label>
+                    <div class="p-profile__imgContainer">
+                        <img src="{{ $user->image ? asset('storage/img/' . $user->image) : asset('img/person.jpg') }}" class="p-profile__img" alt="プロフィール画像">
+                        <input type="file" name="image" class="p-profile__imgInput">
+                    </div>
+                </div>
+            </div>
 
-        @error('email')
-        <div class="p-profile__errMsg c-errMsg">{{ $message }}</div>
-        @enderror
-        <div class="p-profile__part">
-            <label for="" class="p-profile__label">メールアドレス</label>
-            <input type="email" name="email" class="p-profile__input" value="{{ old('email', $user->email) }}">
-        </div>
+            @error('email')
+            <div class="p-profile__errMsg c-errMsg">{{ $message }}</div>
+            @enderror
+            <div class="p-profile__itemContainer">
+                <div class="p-profile__item">
+                    <label for="" class="p-profile__label">メールアドレス</label>
+                    <input type="email" name="email" class="p-profile__input" value="{{ old('email', $user->email) }}">
+                </div>
+            </div>
 
-        @error('bio')
-        <div class="p-profile__errMsg c-errMsg">{{ $message }}</div>
-        @enderror
-        <div class="p-profile__part">
-            <label for="" class="p-profile__label">自己紹介</label>
-            <textarea name="bio" id="" class="p-profile__textarea">{{ old('bio', $user->bio) }}</textarea>
-        </div>
-        <div class="p-profile__btnContainer">
-            <button type="submit" class="c-btn">変更</button>
-        </div>
-    </form>
-
+            @error('bio')
+            <div class="p-profile__errMsg c-errMsg">{{ $message }}</div>
+            @enderror
+            <div class="p-profile__itemContainer">
+                <div class="p-profile__item">
+                    <label for="" class="p-profile__label">自己紹介</label>
+                    <textarea name="bio" id="" class="p-profile__textarea">{{ old('bio', $user->bio) }}</textarea>
+                </div>
+            </div>
+            <div class="p-profile__btnContainer">
+                <button type="submit" class="c-btn">変更</button>
+            </div>
+        </form>
+    </div>
 </div>
 @endsection
