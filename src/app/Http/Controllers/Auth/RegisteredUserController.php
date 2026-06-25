1a86893afacae7dd1807d221480bd6299af9e9a6
shun ogasawara <ogasawarashun49@gmail.com>
2026-06-25 19:21:30 +0900

    ユーザー登録時のニックネーム

--- src/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -38,6 +39,7 @@ class RegisteredUserController extends Controller
         $user = User::create([
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'nickname' => Str::random(8),
         ]);
 
         event(new Registered($user));
